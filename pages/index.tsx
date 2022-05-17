bfeacd5608393d05788a23bedc020b467f6d5e21
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-17 14:36:15 +0000

    Update Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Footer from '../components/Footer'
 import Head from 'next/head'
 import Hola from '../components/Hola'
 import Navbar from '../components/Navbar'
+import Ad from '../components/Ad'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
@@ -17,6 +18,7 @@ const Home: NextPage = () => {
       <Hola message={'We have the ability to build infinite way for us.'} />
       <main className={`md:w-[80%] min-h-[40vh] `}>
         <BlogHeader />
+        <Ad />
       </main>
 
       {/* Footer */}
