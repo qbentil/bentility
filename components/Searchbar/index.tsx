c4e4c4801337565638531aa28da691560549148c
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-17 21:13:27 +0000

    Add Google SearchBar to automate Quick Searches

---
+++ components/Searchbar/index.tsx
@@ -0,0 +1,25 @@
+import { FaSearch } from "react-icons/fa";
+import React from "react";
+
+const SearchBar = () => {
+  return (
+    <div className="w-[95%] md:w-[80%] my-10 p-2 flex justify-center  bg-white">
+      <div className="flex w-full items-center justify-around">
+        <input
+          type="text"
+          name="search"
+          id="search"
+          className="border pr-1 pl-2  md:pl-5 border-gray-300 outline-none w-[80%] md:w-[90%] md:h-10 h-8 md:text-lg text-sm"
+          placeholder="ENHANCED BY Google"
+        />
+        <div className="h-8 md:h-10 w-[15%] md:w-[8%] items-center flex justify-start">
+        <button className="bg-blue-600 p-2 text-white text-sm justify-center  items-center flex  h-[90%] w-full rounded-sm">
+            <FaSearch />
+        </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
