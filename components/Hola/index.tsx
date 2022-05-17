5e16197e1dc0290ad41d06b09a9a2959401d8545
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-17 14:36:07 +0000

    Update Hola Responsioveness

--- components/Hola/index.tsx
+++ components/Hola/index.tsx
@@ -6,7 +6,7 @@ interface Props{
 const Hola:React.FC<Props> = ({message}) => {
   return (
     <div className={`md:w-[80%] flex justify-start  items-center w-[90%]`}>
-        <div className='md:w-[70%] w-[100%] md:text-5xl  font-bold'>
+        <div className='md:w-[90%] lg:w-[80%] w-[100%] md:text-5xl  font-bold'>
             <p className={`md:w-[70%] w-[80%]`}>{message}</p>
         </div>
     </div>
