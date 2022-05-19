b283a9790084e15bab59acb0d28026617bad28ff
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-19 12:19:42 +0000

    Check mobile responsiveness of Categories

--- components/Categories/index.tsx
+++ components/Categories/index.tsx
@@ -6,8 +6,8 @@ import SectionTitle from '../SectionTitle'
 const Categories = () => {
   return (
     <div className='w-full bg-transparent min-h-[20vh]'>
-        <SectionTitle title='Category' />
-        <div className='w-full bg-white grid grid-flow-row grid-cols-6 gap-2 col-start-auto p-2'>
+        <SectionTitle title='Categories' />
+        <div className='w-full bg-white grid grid-flow-row grid-cols-2 gap-1 md:grid-cols-5 lg:grid-cols-7 md:gap-2 col-start-auto p-2'>
         {
             categories.map(category => (
                 <Link  href={`cateory/${category.title}`} key={category.id}>
