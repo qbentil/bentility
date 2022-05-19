f5f7673f69aa8a029ffac274b46d0a3dc858689a
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-19 12:16:02 +0000

    Add dummy categories data

---
+++ data.tsx
@@ -0,0 +1,14 @@
+export const categories = [
+    {
+        id: 1,
+        title: 'React'
+    },
+    {
+        id:2,
+        title: 'JavaScript'
+    },
+    {
+        id:3,
+        title: 'HTML'
+    }
+]
