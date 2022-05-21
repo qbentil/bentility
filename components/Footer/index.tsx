6b2f44d26ffbd715e1d62d146d19fcc85906fa21
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-21 15:06:36 +0000

    Update footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -6,7 +6,10 @@ import { GrLinkedin } from "react-icons/gr";
 import Link from "next/link";
 import React from "react";
 
-const Footer = () => {
+interface Props {
+  nav: boolean;
+}
+const Footer: React.FC<Props> = ({ nav }) => {
   return (
     <div className="border-t-2 border-gray-200 w-[80%] pt-5 flex-col md:flex-row flex justify-around md:justify-center items-center pb-8">
       <div className="md:w-[15%] mb-2 md:mb-0">
@@ -15,17 +18,21 @@ const Footer = () => {
         </p>
       </div>
       <div className="w-full md:w-[60%]  flex items-center justify-around mb-5 md:mb-0">
-        <Link href={"about"}>
-          <p className="text-blue-700 font-thin cursor-pointer">About</p>
-        </Link>
-        <Link href={"contact"}>
-          <p className="text-blue-700 font-thin cursor-pointer">Contact</p>
-        </Link>
-        <Link href={"policy"}>
-          <p className="text-blue-700 font-thin cursor-pointer">
-            Privacy Policy
-          </p>
-        </Link>
+        {!nav && (
+          <>
+            <Link href={"about"}>
+              <p className="text-blue-700 font-thin cursor-pointer">About</p>
+            </Link>
+            <Link href={"contact"}>
+              <p className="text-blue-700 font-thin cursor-pointer">Contact</p>
+            </Link>
+            <Link href={"policy"}>
+              <p className="text-blue-700 font-thin cursor-pointer">
+                Privacy Policy
+              </p>
+            </Link>
+          </>
+        )}
       </div>
       <div className="w-full md:w-[25%] flex items-center justify-around text-gray-500 text-xl mb-2 md:mb-0">
         <a
