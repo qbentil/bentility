b55adf56db92058e9c92fe2409f94da313f2b79c
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-21 05:52:25 +0000

    Re-design 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,25 +1,26 @@
+/* eslint-disable @next/next/no-img-element */
+
+import Link from "next/link";
 import React from "react";
 
 const NotFound = () => {
   return (
     <div className="w-screen h-screen overflow-hidden bg-gray-200 flex items-center justify-center text-gray-700">
       <div className="w-[70%] h-[70%] flex justify-around items-center">
-        <div className="h-full flex flex-col items-center justify-center">
-          <p className="font-bold text-xl">404</p>
-          <p className="capitalize">page not found</p>
-        </div>
-        <div className="h-full bg-gray-800 w-[0.05rem]"></div>
-        <div className="w-[50%] h-full flex justify-center flex-col">
-          <p>
-            The site configured at this address does not contain the requested
-            file.
-          </p>
-          <p>
-            If this is your site, make sure that the filename case matches the
-            URL. For root URLs (like http://example.com/) you must provide an
-            index.html file.
-          </p>
-        </div>
+      <div className="h-10 w-10 md:h-16 md:w-16 lg:h-24 lg:w-24 items-center flex justify-center rounded-full border border-gray-300 mr-2 cursor-pointer">
+            <Link href="/">
+              <img
+                src="https://codersquiz.netlify.app/img/bentil.jpeg"
+                alt="ben"
+                // width={"50px"}
+                // height={"50px"}
+                className="w-[90%] h-[90%] border border-gray-200 rounded-full"
+              />
+            </Link>
+          </div>
+          <div>
+            <p>Page not Found</p>
+          </div>
       </div>
     </div>
   );
