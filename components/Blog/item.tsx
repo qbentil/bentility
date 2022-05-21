fc82f014cdb7139e34ecf07e0ada5c22297e1cbe
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-21 15:34:23 +0000

    Update blog Items

--- components/Blog/item.tsx
+++ components/Blog/item.tsx
@@ -5,11 +5,13 @@ import React from 'react'
 
 const SingleBlog = () => {
   return (
-    <div className='min-h-[15vh] bg-orange-100 border border-orange-300 items-center flex justify-center'>
+    <div className='min-h-[15vh] bg-orange-50 border border-orange-300 items-center flex justify-center'>
       <div className="w-full flex flex-col items-center justify-around min-h-[20vh]">
-        {/* <div className="w-full lg:w-[80%] text-3xl lg:text-5xl  font-bold text-gray-600">
-          <p className={``}>{'title'}</p>
-        </div> */}
+        <div className="w-full lg:w-[80%] text-lg lg:text-xl  font-bold text-gray-600">
+          <p className={``}>
+          How to concat two strings in javascript?
+          </p>
+        </div>
         <div className="flex w-full justify-center items-center">
           <div className="h-6 w-6 md:h-12 md:w-12 lg:h-10 lg:w-10 items-center flex justify-center rounded-full border border-gray-300 mr-2 cursor-pointer">
             <Link href="/">
