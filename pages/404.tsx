939bd06fbc9364fef48feeb3f8d9eca441921c3f
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-21 15:06:23 +0000

    Complete 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
 
+import Footer from "../components/Footer";
 import Link from "next/link";
 import React from "react";
 
 const NotFound = () => {
   return (
-    <div className="w-screen h-screen overflow-hidden bg-gray-200 flex items-center justify-center text-gray-700">
-      <div className="w-[70%] h-[70%] flex justify-around items-center">
+    <div className="w-screen h-screen overflow-hidden bg-gray-200 flex flex-col items-center justify-center text-gray-700">
+      <div className="w-[70%] h-[70%] flex-col flex justify-center items-center">
       <div className="h-10 w-10 md:h-16 md:w-16 lg:h-24 lg:w-24 items-center flex justify-center rounded-full border border-gray-300 mr-2 cursor-pointer">
             <Link href="/">
               <img
@@ -18,10 +19,22 @@ const NotFound = () => {
               />
             </Link>
           </div>
-          <div>
+          <div className="my-3 text-blue-600 font-semibold text-sm md:text-xl font-sans">
+            <p>404 Error</p>
+          </div>
+          <div className="mb-2 text-lg md:text-2xl lg:text-4xl text-black font-bold">
             <p>Page not Found</p>
           </div>
+          <div className="mb-3 text-sm md:text-lg text-gray-500 font-thin">
+            <p>{`Sorry, we didn't find the page you are looking for`}</p>
+          </div>
+          <div className="my-4 text-blue-600 font-semibold">
+            <Link href = "/">
+              Go back home
+            </Link>
+          </div>
       </div>
+      <Footer nav />
     </div>
   );
 };
