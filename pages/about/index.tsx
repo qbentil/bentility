5dfe506757ea725d7e2319ba7894fc292a2e3458
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-06-01 20:24:47 +0000

    Update About Page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,8 +1,13 @@
+import AdBanner from '../../components/Ad'
+import Categories from '../../components/Categories'
 import Footer from '../../components/Footer'
 import Head from 'next/head'
 import Navbar from '../../components/Navbar'
+import Newsletter from '../../components/Newsletter'
 import Pageheader from '../../components/PageHeader'
 import React from 'react'
+import SearchBar from '../../components/Searchbar'
+import { Seperator } from '../../components/Seperator'
 
 const About = () => {
     return (
@@ -13,8 +18,15 @@ const About = () => {
             <link rel="icon" href="/favicon.png" />
           </Head>
           <Navbar />
-          <main className={`md:w-[80%] min-h-[40vh] `}>
-          <Pageheader title='About Us' />
+          <main className={`md:w-[100%] min-h-[40vh]  flex items-center justify-start flex-col`}>
+            <Pageheader title='About Us' />
+            <SearchBar />
+            <div className={`md:w-[80%] w-[95%] min-h-[40vh] `}>
+            <Newsletter />
+            <Seperator width = {"10"} />
+            <AdBanner />
+            <Categories titlePosition='center' />
+            </div>
           </main>
     
           {/* Footer */}
