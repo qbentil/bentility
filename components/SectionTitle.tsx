dba4dbc666ef35c31e47d879d0105a5da0b7964e
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-06-01 20:23:29 +0000

    Make Dynamic SectionTitle Component

--- components/SectionTitle.tsx
+++ components/SectionTitle.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 interface Props {
-    title: string
+    title?: string,
+    tp?:string
 }
 
-const SectionTitle:React.FC<Props> = ({title}) => {
+const SectionTitle:React.FC<Props> = ({title, tp = 'start'}) => {
   return (
-    <div className="my-2 w-full bg-transparent py-2">
+    <div className={`my-2 w-full bg-transparent py-2 flex items-center justify-${tp}`}>
       <p className="text-2xl md:text-3xl font-semibold text-gray-500">{title}</p>
     </div>
   );
