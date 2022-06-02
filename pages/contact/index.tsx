63695bbebdf0f59ffc802ae1975d3aa02f7ee7e0
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-06-02 12:36:39 +0000

    Update Contact Us page

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,28 +1,43 @@
-import BlogHeader from '../../components/BlogHeader'
-import Footer from '../../components/Footer'
-import Head from 'next/head'
-import Hola from '../../components/Hola'
-import Navbar from '../../components/Navbar'
-import Pageheader from '../../components/PageHeader'
-import React from 'react'
+import AdBanner from "../../components/Ad";
+import BlogHeader from "../../components/BlogHeader";
+import Categories from "../../components/Categories";
+import Footer from "../../components/Footer";
+import Form from "../../components/Form";
+import Head from "next/head";
+import Hola from "../../components/Hola";
+import Navbar from "../../components/Navbar";
+import Newsletter from "../../components/Newsletter";
+import Pageheader from "../../components/PageHeader";
+import React from "react";
+import SearchBar from "../../components/Searchbar";
+import { Seperator } from "../../components/Seperator";
 
 const Contact = () => {
-    return (
-        <div className={'items-center flex flex-col'}>
-          <Head>
-            <title>Bentility | Contact</title>
-            <meta name="description" content="Bentil's Blog| Bentility" />
-            <link rel="icon" href="/favicon.png" />
-          </Head>
-          <Navbar />
-          <main className={`md:w-[80%] min-h-[40vh] `}>
-          <Pageheader title='Contact Us' />
-          </main>
-    
-          {/* Footer */}
-          <Footer nav />
+  return (
+    <div className={"items-center flex flex-col"}>
+      <Head>
+        <title>Bentility | Contact</title>
+        <meta name="description" content="Bentil's Blog| Bentility" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <Navbar />
+      <main
+        className={`md:w-[100%] min-h-[40vh]  flex items-center justify-start flex-col`}
+      >
+        <Pageheader title="Contact Us" />
+        <SearchBar />
+        <div className={`md:w-[80%] w-[95%] min-h-[40vh] `}>
+          <Form />
+          <Seperator width={"10"} />
+          <AdBanner />
+          <Categories titlePosition="center" />
         </div>
-      )
-}
+      </main>
 
-export default Contact
+      {/* Footer */}
+      <Footer nav />
+    </div>
+  );
+};
+
+export default Contact;
