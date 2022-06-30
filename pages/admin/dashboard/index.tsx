2e22cbb20cdcaf0e3925e0f50e5bc014a4dfeea6
qbentil <bentilshadrack72@gmail.com>
2022-06-30 16:56:30 +0000

    Create Admin ashboard

---
+++ pages/admin/dashboard/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Head>
+        <title>Bentility| Admin</title>
+        <meta name="description" content="Bentil's Blog| Bentility" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+    </div>
+  );
+};
+
+export default Dashboard;
