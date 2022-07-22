ea13e7b26af94b3960163f3089036ebc323b3547
qbentil <bentilshadrack72@gmail.com>
2022-07-22 18:31:11 +0000

    Update Component [Admin]

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,14 +1,15 @@
-import Head from "next/head";
 import React, { useState } from "react";
+
 import Auth from "./auth";
 import Dashboard from "./dashboard";
+import Head from "next/head";
 
 const Admin = () => {
     const [isLoggedin, setIsLoggedin] = useState(false);
   return (
     <div>
         {
-            isLoggedin ? <Dashboard />: <Auth />
+            !isLoggedin ? <Dashboard />: <Auth />
         }
     </div>
   );
