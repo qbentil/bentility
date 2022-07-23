1fba5f770e4328f8249d3109641816d4ed12e73b
qbentil <bentilshadrack72@gmail.com>
2022-07-23 15:28:00 +0000

    Update sidenave

--- components/Admin/sidenav/index.tsx
+++ components/Admin/sidenav/index.tsx
@@ -5,6 +5,7 @@ import { FaClipboardList } from "react-icons/fa";
 import { CgListTree } from "react-icons/cg";
 import { BsCalendarEvent } from "react-icons/bs";
 import { BiLogOutCircle } from "react-icons/bi";
+import { AiOutlineAppstoreAdd } from "react-icons/ai";
 const Navs = [
   {
     name: "Dashboard",
@@ -31,6 +32,11 @@ const Navs = [
     icon: <BsCalendarEvent />,
     link: "/archives",
   },
+  {
+    name: "Logout",
+    icon: <BiLogOutCircle />,
+    link: "/logout",
+  }
 ];
 
 function Sidenav({ page }: { page: string }) {
@@ -41,7 +47,7 @@ function Sidenav({ page }: { page: string }) {
           {/* Navigations */}
           {Navs.map((nav, index) => (
             <Link href={`/admin/${nav.link}`} key={index}>
-              <div className={`flex gap-2 items-center cursor-pointer ${page === nav.name? "hover:text-[#5C6E9A] hover:bg-transparent bg-[#EB2E64] text-white":"text-[#5C6E9A] hover:bg-[#EB2E64] hover:text-white"} py-2 px-5 w-full rounded-full transition-all duration-75 ease-in-out`}>
+              <div className={`flex gap-4 items-center cursor-pointer ${page === nav.name? "hover:text-white hover:bg-primary bg-active-bg text-active":"text-active hover:bg-active-bg hover:text-active"} py-2 px-5 w-full rounded-full transition-all duration-75 ease-in-out`}>
                 {nav.icon}
                 <p>{nav.name}</p>
               </div>
@@ -50,9 +56,9 @@ function Sidenav({ page }: { page: string }) {
         </div>
         <div className=" w-full flex flex-col mb-5">
           <Link href="/admin">
-            <div className="flex gap-3 items-center cursor-pointer bg-[#EB2E64] text-white hover:bg-transparent hover:text-[#5C6E9A] py-2 px-5 w-full rounded-full transition-all duration-75 ease-in-out">
-              <BiLogOutCircle />
-              <p>Logout</p>
+            <div className="flex gap-3 justify-center items-center cursor-pointer bg-primary text-white hover:bg-active-bg hover:text-active py-2 px-5 w-full rounded-full transition-all duration-75 ease-in-out">
+              <AiOutlineAppstoreAdd />
+              <p>New Post</p>
             </div>
           </Link>
         </div>
