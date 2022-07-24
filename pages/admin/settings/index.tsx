a593ff3b0928e00f4e39e83030e42d3546cd97a1
qbentil <bentilshadrack72@gmail.com>
2022-07-24 20:59:35 +0000

    Update settings page

--- pages/admin/settings/index.tsx
+++ pages/admin/settings/index.tsx
@@ -1,23 +1,11 @@
-import Head from "next/head";
-import React from "react";
-import { Navbar, Sidenav } from "../../../components/Admin";
+import { useState } from "react";
+import Admin from "..";
+import Settings from "./__auth";
 
-const Settings = () => {
+const Page = () => {
   return (
-    <div>
-      <Head>
-        <title>Bentility| Admin</title>
-        <meta name="description" content="Bentility Admin | Settings" />
-        <link rel="icon" href="/favicon.png" />
-      </Head>
-      <main className="bg-gray-200 w-screen h-screen ">
-        <div className="">
-          <Navbar />
-          <Sidenav page="Settings" />
-        </div>
-      </main>
-    </div>
+    <Admin page={<Settings />} />
   );
 };
 
-export default Settings;
+export default Page;
