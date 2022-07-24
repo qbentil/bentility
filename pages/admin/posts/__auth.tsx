ab443b351bc552c7f76f1541c195e23f4f0b9892
qbentil <bentilshadrack72@gmail.com>
2022-07-24 21:01:10 +0000

    Posts auth

---
+++ pages/admin/posts/__auth.tsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import React from "react";
+import { Navbar, Sidenav } from "../../../components/Admin";
+
+const Posts = () => {
+  return (
+    <div>
+      <Head>
+        <title>Bentility| Admin</title>
+        <meta name="description" content="Bentility Admin | Post" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <main className="bg-gray-200 w-screen h-screen ">
+        <div className="">
+          <Navbar />
+          <Sidenav page="Posts" />
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Posts;
