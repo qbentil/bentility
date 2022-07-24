5e1198ef5cf673d50066df07062a8720735f5c00
qbentil <bentilshadrack72@gmail.com>
2022-07-24 21:00:10 +0000

    Update Dashboard page

--- pages/admin/dashboard/index.tsx
+++ pages/admin/dashboard/index.tsx
@@ -1,27 +1,11 @@
-import Head from "next/head";
-import React from "react";
-import { Navbar, PopularPosts, Sidenav, Tabs } from "../../../components/Admin";
+import { useState } from "react";
+import Admin from "..";
+import Dashboard from "./__auth";
 
-const Dashboard = () => {
+const Page = () => {
   return (
-    <div>
-      <Head>
-        <title>Bentility| Admin</title>
-        <meta name="description" content="Bentility Admin | Dashboard" />
-        <link rel="icon" href="/favicon.png" />
-      </Head>
-      <main className="bg-[#f8f8f8] w-screen h-screen ">
-        <Navbar />
-        <div className="flex">
-          <Sidenav page="Dashboard" />
-          <div className="min-h-[90vh] h-[90vh] overflow-y-auto w-[80%] py-5">
-            <Tabs />
-            <PopularPosts />
-          </div>
-        </div>
-      </main>
-    </div>
+    <Admin page={<Dashboard />} />
   );
 };
 
-export default Dashboard;
+export default Page;
