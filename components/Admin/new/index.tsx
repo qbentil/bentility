24ae658ffb9b32cef27a8984add44dd596a0c27c
qbentil <bentilshadrack72@gmail.com>
2022-07-29 12:17:51 +0000

    Update New Post COmponent

--- components/Admin/new/index.tsx
+++ components/Admin/new/index.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import {useState} from "react";
 import {BiAddToQueue} from 'react-icons/bi'
 import CategorySelector from "./categorySelector";
 import CustomEditor from "./editor";
 function NewPost() {
+  const [categories, setCategories] = useState([{value: "", label: ""}]);
+  const [body, setBody] = useState('');
+  const [title, setTitle] = useState('');
+
+  // handle category change
+  const handleCategoryChange = (selectedCategories: any) => {
+    setCategories(selectedCategories);
+    
+  }
   return (
     <div className="w-full h-full bg-tansparent">
-      <form action="" method="post">
+      <form action="" method="post" autoComplete="off" autoCorrect="on" autoSave="on">
         <h1 className="font-bold text-xl uppercase text-active font-sans mb-4">
           Create a New Post
         </h1>
@@ -19,6 +28,8 @@ function NewPost() {
           <input
             type="text"
             id="large-input"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
             className="block p-2 px-4 w-full text-gray-900 bg-gray-50 rounded-lg outline-none"
             placeholder="Title"
           />
@@ -30,7 +41,7 @@ function NewPost() {
           >
             Category
           </label>
-          <CategorySelector className="block p-4 w-full text-gray-900 bg-gray-50 rounded-lg outline-none" />
+          <CategorySelector onChange={handleCategoryChange} />
         </div>
         <div className="flex flex-col mb-3">
           <label
@@ -39,7 +50,7 @@ function NewPost() {
           >
             Body
           </label>
-          <CustomEditor />
+          <CustomEditor val={body} setVal={setBody} />
         </div>
         <div className="flex flex-col mb-3 items-end">
             {/* submit button */}
