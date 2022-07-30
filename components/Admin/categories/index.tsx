174e141dfcd21a36fa5f8bcb5929916eeaa03adf
qbentil <bentilshadrack72@gmail.com>
2022-07-30 18:45:44 +0000

    Categories COmponent

---
+++ components/Admin/categories/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Unit from './unit'
+function Categories() {
+  return (
+    <div>
+        <h1>Categories</h1>
+        <Unit />
+    </div>
+  )
+}
+
+export default Categories
