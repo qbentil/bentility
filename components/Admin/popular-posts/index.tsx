b3762e590288ceebc2b48ac1d365e35a4578ee99
qbentil <bentilshadrack72@gmail.com>
2022-07-30 18:44:47 +0000

    Update popular posts

--- components/Admin/popular-posts/index.tsx
+++ components/Admin/popular-posts/index.tsx
@@ -4,21 +4,22 @@ import Post from '../post'
 
 function PopularPosts() {
   return (
-    <div className='w-full flex flex-col items-center px-5 my-4'>
+    <div className='w-[60%] flex flex-col items-center px-5 my-4'>
         {/* section head */}
         <div className='bg-white w-full rounded-t-lg flex justify-between items-center py-2 px-4 border-b-2 border-gray-200'>
             <p className=' text-active font-sans'>Popular Posts</p>
             <div className='flex items-center justify-center gap-3 border border-active-bg py-2 px-4 font-sans cursor-pointer'>
                 <RiSoundModuleLine className='text-primary' />
-                <p className=' text-active text-sm'>View</p>
+                <p className=' text-active text-sm'>View All</p>
             </div>
         </div>
         {/* posts table */}
-        <div className='w-full flex flex-col bg-transparent pt-2 gap-2'>
+        <div className='w-full flex flex-col bg-white pt-2 gap-2 overflow-y-auto h-[59vh]'>
             <Post />
             <Post />
             <Post />
             <Post />
+
         </div>
     </div>
   )
