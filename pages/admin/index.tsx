0d774e4369e40623c58a58eaecf16892c795f08a
qbentil <bentilshadrack72@gmail.com>
2022-08-27 15:48:21 +0000

    Update home dashboard

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -3,8 +3,9 @@ import React, { useEffect, useState } from "react";
 import Auth from "./auth";
 import Dashboard from "./dashboard";
 import { useStateValue } from "../../context/StateProvider";
+import { NextPage } from "next";
 
-const Admin = ({page = <Dashboard />}: {page?:JSX.Element}) => {
+const Admin: NextPage = ({page = <Dashboard />}: {page?:JSX.Element}) => {
     const [{user}, dispatch] = useStateValue();
     useEffect(()=> {
       console.log("user", user);
