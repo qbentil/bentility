b67beada5f735baf9c85ca4af26f4a423e69f94b
qbentil <bentilshadrack72@gmail.com>
2022-08-28 00:30:53 +0000

    Remove test log

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,15 +1,10 @@
-import React, { useEffect, useState } from "react";
 
 import Auth from "./auth";
 import Dashboard from "./dashboard";
-import { useStateValue } from "../../context/StateProvider";
-import { NextPage } from "next";
+import { useStateValue } from "../../context/StateProvider"
 
 const Admin = ({page = <Dashboard />}: {page?:JSX.Element}) => {
     const [{user}, dispatch] = useStateValue();
-    useEffect(()=> {
-      console.log("user", user);
-    })
   return (
     <div>
         {
