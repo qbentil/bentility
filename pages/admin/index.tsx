f49c2f1aa07c4085956ae8c391351db264eabc5c
qbentil <bentilshadrack72@gmail.com>
2022-08-28 13:47:28 +0000

    Get user on Start

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -2,9 +2,26 @@
 import Auth from "./auth";
 import Dashboard from "./dashboard";
 import { useStateValue } from "../../context/StateProvider"
+import { useEffect } from "react";
+import { GET_SESSION_USER } from "../../util/session";
+import { User } from "../../types";
 
 const Admin = ({page = <Dashboard />}: {page?:JSX.Element}) => {
     const [{user}, dispatch] = useStateValue();
+    useEffect(() => {
+      const fetchSession = async () => {
+        // retrun if user is already logged in
+        if (user) return;
+          const session_user:User = await GET_SESSION_USER()
+        if(session_user){
+          dispatch({
+            type: "SET_USER",
+            user:session_user
+          })
+        }
+      };
+      fetchSession();
+    }, []);
   return (
     <div>
         {
