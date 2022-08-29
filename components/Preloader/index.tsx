dd076ea8cb12e9f5f8c771a1aa3f360b45e3c5e9
qbentil <bentilshadrack72@gmail.com>
2022-08-29 13:52:14 +0000

    Create page preloader

---
+++ components/Preloader/index.tsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import { GrCloudDownload } from "react-icons/gr";
+const Preloader = () => {
+  return (
+    <div>
+      <Head>
+        <title>Bentility| Admin Login</title>
+        <meta name="description" content="Bentil's Blog | Bentility" />
+        <link rel="icon" href="/favicon.png" />
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css"
+        />
+      </Head>
+      <main className="w-screen h-screen bg-active-bg flex items-center justify-center ">
+        <div className="animate-pulse text-2xl flex items-center justify-center gap-x-3">
+          <GrCloudDownload className="animate animate-bounce text-active" />
+          <p className="text-xl">Loading!.....</p>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Preloader;
