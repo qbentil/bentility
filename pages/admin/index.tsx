6ed41989d6e2126e02fd3a45f150386f85b8cb48
qbentil <bentilshadrack72@gmail.com>
2022-08-29 13:52:45 +0000

    Update Admin Root

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -2,33 +2,36 @@
 import Auth from "./auth";
 import Dashboard from "./dashboard";
 import { useStateValue } from "../../context/StateProvider"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { GET_SESSION_USER } from "../../util/session";
 import { User } from "../../types";
+import Preloader from "../../components/Preloader";
 
 const Admin = ({page = <Dashboard />}: {page?:JSX.Element}) => {
     const [{user}, dispatch] = useStateValue();
+    const [loading, setLoading] = useState(false);
     useEffect(() => {
       const fetchSession = async () => {
         // retrun if user is already logged in
-        if (user) return;
+        if (user) {
+          return;
+        };
+        setLoading(true);
           const session_user:User = await GET_SESSION_USER()
+
         if(session_user){
           dispatch({
             type: "SET_USER",
             user:session_user
           })
         }
+        setLoading(false)
       };
       fetchSession();
     }, []);
-  return (
-    <div>
-        {
-            !user ? <Auth />: page
-        }
-    </div>
-  );
+
+    if(loading) return <Preloader />
+    return  user ? page : <Auth />
 };
 
 export default Admin;
