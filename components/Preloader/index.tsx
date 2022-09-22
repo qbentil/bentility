cf5e2b53319b5977273ab05f9c2c63f41ebaab8a
qbentil <bentilshadrack72@gmail.com>
2022-09-22 12:57:44 +0000

    Update Preloader icon

--- components/Preloader/index.tsx
+++ components/Preloader/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { GrCloudDownload } from "react-icons/gr";
+import { BiLoaderCircle } from "react-icons/bi";
 const Preloader = () => {
   return (
     <div>
@@ -14,7 +14,7 @@ const Preloader = () => {
       </Head>
       <main className="w-screen h-screen bg-active-bg flex items-center justify-center ">
         <div className="animate-pulse text-2xl flex items-center justify-center gap-x-3">
-          <GrCloudDownload className="animate animate-bounce text-active" />
+          <BiLoaderCircle className="animate animate-spin text-active" />
           <p className="text-xl">Loading!.....</p>
         </div>
       </main>
