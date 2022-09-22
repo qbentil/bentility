828191a52da9eed917f29a9be050afe99a98dcdb
qbentil <bentilshadrack72@gmail.com>
2022-09-22 12:59:07 +0000

    Update Preloader icon

--- components/Preloader/index.tsx
+++ components/Preloader/index.tsx
@@ -7,15 +7,11 @@ const Preloader = () => {
         <title>Bentility| Admin Login</title>
         <meta name="description" content="Bentil's Blog | Bentility" />
         <link rel="icon" href="/favicon.png" />
-        <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css"
-        />
       </Head>
       <main className="w-screen h-screen bg-active-bg flex items-center justify-center ">
         <div className="animate-pulse text-2xl flex items-center justify-center gap-x-3">
           <BiLoaderCircle className="animate animate-spin text-active" />
-          <p className="text-xl">Loading!.....</p>
+          <p className="text-xl text-active">Loading!.....</p>
         </div>
       </main>
     </div>
