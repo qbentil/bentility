1438f21b91f1aff4d711931624fb8884d9fe0e5a
qbentil <bentilshadrack72@gmail.com>
2022-09-23 22:27:55 +0000

    Update param

--- components/Admin/new/editor.tsx
+++ components/Admin/new/editor.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react'
 
-function CustomEditor({val, setVal}: {val:string, setVal:any}) {
+function CustomEditor({val, setVal}: {val:string, setVal:(e:any) => void}) {
   return (
     <textarea name="body" id="body" cols={30} rows={10} className='block p-2 px-4 w-full text-gray-900 bg-gray-50 rounded-lg outline-none' value={val} onChange ={setVal} ></textarea>
   )
