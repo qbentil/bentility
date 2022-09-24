19aeb894cabf6bf2ff1ca57b2230c4b0ca2c279d
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-09-24 08:22:57 +0000

    feat: :sparkles: toggle functionality

--- components/Admin/categories/index.tsx
+++ components/Admin/categories/index.tsx
@@ -1,42 +1,56 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Unit from './unit'
 import Link from 'next/link'
 import { CgListTree } from 'react-icons/cg'
+import { BsTable } from 'react-icons/bs'
+import { RiLayoutGridFill } from 'react-icons/ri'
 import { useStateValue } from '../../../context/StateProvider'
 import { Category } from '../../../types'
+import Button from '../../Button'
 
 function Categories() {
-const [{categories}, dispatch] = useStateValue()
+  const [{ categories }, dispatch] = useStateValue()
+  const [view, setView] = useState('card')
 
-  return (
-    <div className='bg-white p-5 max-h-[90vh] h-full'>
-        <div className="bg-transparent w-full flex items-center justify-between px-5 mb-5 py-2 rounded-lg ">
-          <h3 className="text-2xl md:text-3xl font-semibold text-primary poppins">
-            Categories
-          </h3>
-          {/* New Category button */}
-          <div className="">
-          <Link href="/admin/categories/new">
-            <div className={`flex gap-3 justify-center items-center cursor-pointer bg-primary text-white hover:bg-active-bg hover:text-active py-2 px-5 w-full rounded-full transition-all duration-75 ease-in-out`}>
-              <CgListTree />
-              <p>Add New</p>
-            </div>
+  const changeView = (view: string) => { 
+    setView(view)
+  }
+
+	return (
+		<div className='bg-white p-5 max-h-[90vh] h-full poppins'>
+			<div className='bg-transparent w-full flex items-center justify-between px-5 mb-5 py-2 rounded-lg '>
+				<h3 className='text-2xl md:text-3xl font-semibold text-primary poppins'>
+					Categories
+				</h3>
+				{/* New Category button */}
+				<div className='flex gap-4 '>
+					<Link href='/admin/categories/new'>
+						<Button text='New ' icon={<CgListTree />} />
           </Link>
-        </div>
-        </div>
-      <div className='grid grid-cols-4 gap-7'>
-        
           {
-            categories.map((category:Category) => (
-              <Unit key={category._id} data={category} />
-            ))
+            view === 'card' ? (
+              <Button text='Table' icon={<BsTable />} onClick={() => changeView('table')} />
+            ) : (
+              <Button text='Card' icon={<RiLayoutGridFill />} onClick={() => changeView('card')} />
+            )
           }
-        
-        
-
-        </div>
-    </div>
-  )
+          
+				</div>
+      </div>
+      {
+        view === 'card' ? (
+          <div className='grid grid-cols-2 lg:grid-cols-4 gap-7'>
+				{categories.map((category: Category) => (
+					<Unit key={category._id} data={category} />
+				))}
+			</div>
+        ) : (
+            <div>Table</div>
+        )
+      }
+			
+		</div>
+	)
 }
 
 export default Categories
