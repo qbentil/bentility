973d5def5febb88cd860173285413ce0c3b2ad48
qbentil <bentilshadrack72@gmail.com>
2022-09-24 11:35:36 +0000

    Update Custom editor focus

--- components/Admin/new/editor.tsx
+++ components/Admin/new/editor.tsx
@@ -3,8 +3,7 @@ import React from 'react'
 
 function CustomEditor({val, setVal}: {val:string, setVal:(e:any) => void}) {
   return (
-
-    <textarea name="body" id="body" cols={30} rows={12} className='block p-2 px-4 w-full border border-gray-300 text-gray-900 rounded-lg outline-none resize-none' value={val} onChange ={(e) => setVal(e.target.body)}></textarea>
+    <textarea placeholder="Content...." name="body" id="body" cols={30} rows={12} className='block p-2 px-4 w-full border-2 border-gray-300 text-gray-900 rounded-lg outline-none resize-none focus:border-primary' value={val} onChange ={(e) => setVal(e.target.value)}></textarea>
   )
 }
 
