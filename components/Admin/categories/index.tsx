6b8ce9731d3f25b5f4cc43d6a3a204df3752166d
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-09-24 10:04:56 +0000

    feat: :sparkles: implemented toggle functionality

--- components/Admin/categories/index.tsx
+++ components/Admin/categories/index.tsx
@@ -3,21 +3,22 @@ import Unit from './unit'
 import Link from 'next/link'
 import { CgListTree } from 'react-icons/cg'
 import { BsTable } from 'react-icons/bs'
-import { RiLayoutGridFill } from 'react-icons/ri'
+import { RiLayoutGridFill, RiSoundModuleLine } from 'react-icons/ri'
 import { useStateValue } from '../../../context/StateProvider'
 import { Category } from '../../../types'
 import Button from '../../Button'
+import Tableunit from './tableunit'
 
 function Categories() {
-  const [{ categories }, dispatch] = useStateValue()
-  const [view, setView] = useState('card')
+	const [{ categories }, dispatch] = useStateValue()
+	const [view, setView] = useState('card')
 
-  const changeView = (view: string) => { 
-    setView(view)
-  }
+	const changeView = (view: string) => {
+		setView(view)
+	}
 
 	return (
-		<div className='bg-white p-5 max-h-[90vh] h-full poppins'>
+		<div className='bg-white p-5 max-h-[90vh] h-full poppins overflow-y-scroll'>
 			<div className='bg-transparent w-full flex items-center justify-between px-5 mb-5 py-2 rounded-lg '>
 				<h3 className='text-2xl md:text-3xl font-semibold text-primary poppins'>
 					Categories
@@ -26,29 +27,39 @@ function Categories() {
 				<div className='flex gap-4 '>
 					<Link href='/admin/categories/new'>
 						<Button text='New ' icon={<CgListTree />} />
-          </Link>
-          {
-            view === 'card' ? (
-              <Button text='Table' icon={<BsTable />} onClick={() => changeView('table')} />
-            ) : (
-              <Button text='Card' icon={<RiLayoutGridFill />} onClick={() => changeView('card')} />
-            )
-          }
-          
+					</Link>
+					{view === 'card' ? (
+						<Button
+							text='Table'
+							icon={<BsTable />}
+							onClick={() => changeView('table')}
+						/>
+					) : (
+						<Button
+							text='Card'
+							icon={<RiLayoutGridFill />}
+							onClick={() => changeView('card')}
+						/>
+					)}
 				</div>
-      </div>
-      {
-        view === 'card' ? (
-          <div className='grid grid-cols-2 lg:grid-cols-4 gap-7'>
-				{categories.map((category: Category) => (
-					<Unit key={category._id} data={category} />
-				))}
 			</div>
-        ) : (
-            <div>Table</div>
-        )
-      }
-			
+			{view === 'card' ? (
+				<div className='grid grid-cols-2 lg:grid-cols-4 gap-7'>
+					{categories.map((category: Category) => (
+						<Unit key={category._id} data={category} />
+					))}
+				</div>
+			) : (
+				<div className='w-full h-full pr-4 mb-20'>
+					<div>
+				
+						{categories.map((category: Category) => (
+							<Tableunit key={category._id} data={category} />
+						))}
+						
+					</div>
+				</div>
+			)}
 		</div>
 	)
 }
