81f6464371d8c5a580fd6e272e46261f2a4e853e
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-09-25 14:15:37 +0000

    feat: :sparkles: dynamic categories list

--- components/Admin/popular-categories/index.tsx
+++ components/Admin/popular-categories/index.tsx
@@ -1,24 +1,33 @@
+import Link from 'next/link'
 import React from 'react'
 import { RiSoundModuleLine } from 'react-icons/ri'
+import { useStateValue } from '../../../context/StateProvider'
 
 import Unit from './unit'
 
 function PopularCategories() {
+  const [{categories}, dispatch] = useStateValue()
   return (
     <div className='w-[40%] flex flex-col items-center px-5 my-4'>
         {/* section head */}
-        <div className='bg-white w-full rounded-t-lg flex justify-between items-center py-2 px-4 border-b-2 border-gray-200'>
+      <div className='bg-white w-full rounded-t-lg flex justify-between items-center py-2 px-4 border-b-2 border-gray-200'>
             <p className=' text-active font-sans'>Popular Categories</p>
-            <div className='flex items-center justify-center gap-3 border border-active-bg py-2 px-4 font-sans cursor-pointer'>
+        <Link href='/admin/categories'>
+            <div className='flex items-center justify-center gap-3 border border-active-bg py-2 px-4 font-sans cursor-pointer hover:bg-active-bg ease-in-out duration-300 transition-colors'>
                 <RiSoundModuleLine className='text-primary' />
                 <p className=' text-active text-sm'>View All</p>
-            </div>
+          </div>
+        </Link>
         </div>
         {/* posts table */}
         <div className='w-full flex flex-col bg-white pt-2 gap-2'>
-            <Unit />
-            <Unit />
-            <Unit />
+        {
+					
+					categories &&
+						categories
+							.slice(0, 8)
+							.map((cat : any) => <Unit data={cat} key={cat._id} />)
+				}
         </div>
     </div>
   )
--- components/Admin/popular-categories/unit.tsx
+++ components/Admin/popular-categories/unit.tsx
@@ -1,23 +1,53 @@
-import React from 'react'
-import {FaClipboardList} from 'react-icons/fa'
-const Unit = () => {
-  return (
-    <div className='w-full  flex items-center justify-between py-2 border-b-2 border-gray-200 px-4 hover:bg-active-bg group cursor-pointer transition-all ease-in-out duration-75'>
-        <div className='flex items-center w-[80%] gap-8 font-sans '>
-            <div className='w-20 h-10 flex justify-center text-yellow-700 items-center bg-yellow-200 rounded-sm group-hover:bg-white'>JS</div>
-            <div className="flex flex-col mr-10">
-                <h2 className='text-active'>JavaScript</h2>
-                <p className='text-[0.7rem] text-[#9F9F9F]'>24 July, 2022</p>
-            </div>
-        </div>
-        <div className='flex items-center justify-center gap-5'>
-            <div className="flex items-center justify-center gap-2 font-sans">
-                <FaClipboardList className='text-[#6E6E6E]' />
-                <p className='text-[#4B4B4B] text-[0.9rem]'>1200</p>
-            </div>
-        </div>
-    </div>
-  )
+import React, { useState } from 'react'
+import { FaClipboardList } from 'react-icons/fa'
+import { Category } from '../../../types'
+import { ColorOpacity } from '../../../util/functions'
+
+const Unit = ({ data }: { data: Category }) => {
+	const initials =
+		data.title.split(' ').length > 1
+			? data.title
+					.split(' ')
+					.map((word: string) => word.charAt(0).toUpperCase())
+					.join('')
+			: data.title.charAt(0).toUpperCase() +
+            data.title.charAt(1).toUpperCase()
+    
+            const shortDate = (word: any) => { 
+                const date = new Date(word)
+                const month = date.toLocaleDateString('default', { month: 'short' })
+                const day = date.getDate()
+                const year = date.getFullYear()
+                const formattedDate = `${day} ${month}, ${year}`
+                return formattedDate
+    }
+    
+    const [date, setDate] = useState<string>(shortDate(data.createdAt))
+
+	return (
+		<div className='w-full  flex items-center justify-between py-2 border-b-2 border-gray-200 px-4 hover:bg-active-bg group cursor-pointer transition-all ease-in-out duration-75'>
+			<div className='flex items-center w-[80%] gap-8 font-sans '>
+				<div className='w-20 h-10 flex justify-center items-center rounded-md group-hover:bg-white' style={{
+                    backgroundColor: ColorOpacity(data.color || '', 20),
+                    color: data.color || 'black'
+					}}>
+					{initials}
+				</div>
+				<div className='flex flex-col mr-10'>
+					<h2 className='text-active'>{data.title}</h2>
+					<p className='text-[0.7rem] text-[#9F9F9F]'>
+						{date}
+					</p>
+				</div>
+			</div>
+			<div className='flex items-center justify-center gap-5'>
+				<div className='flex items-center justify-center gap-2 font-sans'>
+					<FaClipboardList className='text-[#6E6E6E]' />
+					<p className='text-[#4B4B4B] text-[0.9rem]'>1200</p>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Unit
