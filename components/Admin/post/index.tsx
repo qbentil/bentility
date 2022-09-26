b50bf3257002141f21ee3f6ecc1c840b80ccd3cb
qbentil <bentilshadrack72@gmail.com>
2022-09-26 03:22:36 +0000

    Refactor posts, make use of custom functions

--- components/Admin/post/index.tsx
+++ components/Admin/post/index.tsx
@@ -1,56 +1,46 @@
-import React, { useEffect, useState } from 'react'
-import { BsBook, BsEye } from 'react-icons/bs'
-import { Post } from '../../../types'
+import React, { useEffect, useState } from "react";
+import { BsBook, BsEye } from "react-icons/bs";
+import { MdDateRange } from "react-icons/md";
+import { Post } from "../../../types";
+import { convertDate, generateInitials } from "../../../util/functions";
 
-import { Writer, Categories } from '../posts'
+import { Writer, Categories } from "../posts";
 
 const Post = ({ post }: { post: Post }) => {
-	const names = post.title.split(' ')
-	const initials =
-		names[0].charAt(0).toUpperCase() +
-		names[names.length - 1].charAt(0).toUpperCase()
 
-	
-    
-    // funtion to return short date
-    const shortDate = (word: any) => { 
-        const date = new Date(word)
-        const month = date.toLocaleDateString('default', { month: 'short' })
-        const day = date.getDate()
-        const year = date.getFullYear()
-        const formattedDate = `${day} ${month}, ${year}`
-        return formattedDate
-    }
+  return (
+    <div className="w-full  flex items-center justify-between py-2 border-b-2 border-gray-200 px-4 hover:bg-active-bg group cursor-pointer transition-all ease-in-out duration-75 ">
+      <div className="flex items-center w-[90%] gap-8 font-sans ">
+        <div className="w-10 h-10 flex justify-center text-primary items-center bg-active-bg rounded-full group-hover:bg-white">
+          {generateInitials(post.title || '')}
+        </div>
+        <div className="flex flex-col mr-10 w-[40%]">
+          <h2 className="text-active truncate">{post.title}</h2>
+          <div className="flex items-center justify-center gap-x-2">
+            <Writer
+              id={post?.writer || "N/A"}
+              className={"text-[0.7rem] text-gray-500 flex gap-2 items-center"}
+            />
+            <div className="flex items-center justify-center gap-x-2">
+              <MdDateRange className="text-[0.7rem] text-primary" />
+              <p className="text-[0.7rem] text-[#4B4B4B] flex items-center gap-2">
+                {convertDate(post.createdAt || "", "short")}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className=" flex max-w-[55%] overflow-auto gap-2 scrollbar-hidden mx-auto">
+          <Categories ids={post.categories} />
+        </div>
+      </div>
+      <div className="flex items-center justify-center gap-5 w-[10%]">
+        <div className="flex items-center justify-center gap-2 font-sans">
+          <BsEye className="text-[#6E6E6E]" />
+          <p className="text-[#4B4B4B] text-[0.9rem]">{post.views}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-	const [date, setDate] = useState<string>(shortDate(post.createdAt))
-	return (
-		<div className='w-full  flex items-center justify-between py-2 border-b-2 border-gray-200 px-4 hover:bg-active-bg group cursor-pointer transition-all ease-in-out duration-75 '>
-			<div className='flex items-center w-[80%] gap-8 font-sans '>
-				<div className='w-10 h-10 flex justify-center text-primary items-center bg-active-bg rounded-full group-hover:bg-white'>
-					{initials}
-				</div>
-				<div className='flex flex-col mr-10'>
-					<h2 className='text-active truncate'>{post.title}</h2>
-                    
-                    <Writer id={post?.writer || ''} className={'text-[0.7rem] text-gray-500 flex gap-2 items-center'} />
-                    
-				</div>
-                <div className=' flex max-w-[25%] overflow-auto gap-2 scrollbar-hidden mx-auto'>
-                    
-					<Categories ids={post.categories} />
-				</div>
-			</div>
-			<div className='flex items-center justify-center gap-5'>
-				<div className='flex items-center justify-center gap-2 font-sans'>
-					<p className='text-[#4B4B4B] text-xs'>{date}</p>
-				</div>
-				<div className='flex items-center justify-center gap-2 font-sans'>
-					<BsEye className='text-[#6E6E6E]' />
-					<p className='text-[#4B4B4B] text-[0.9rem]'>{post.views}</p>
-				</div>
-			</div>
-		</div>
-	)
-}
-
-export default Post
+export default Post;
