9dc19a287432c6769185d4998f72f9e8abf1f095
qbentil <bentilshadrack72@gmail.com>
2022-09-27 18:11:26 +0000

    Setup settings page

--- admin/settings/index.tsx
+++ admin/settings/index.tsx
@@ -1,19 +1,25 @@
 import Head from "next/head";
 import React from "react";
 import { Navbar, Sidenav } from "../../components/Admin";
+import SettingsView from "../../components/settings";
 
 const Settings = () => {
   return (
     <div>
       <Head>
-        <title>Bentility| Admin</title>
+        <title>Bentility| Admin - Settings</title>
         <meta name="description" content="Bentility Admin | Settings" />
         <link rel="icon" href="/favicon.png" />
       </Head>
       <main className="bg-gray-200 w-screen h-screen ">
         <div className="">
           <Navbar />
-          <Sidenav page="Settings" />
+          <div className="flex">
+            <Sidenav page="Settings" />
+            <div className="min-h-[90vh] h-[90vh] overflow-y-auto w-[80%] py-3 px-5">
+              <SettingsView />
+            </div>
+          </div>
         </div>
       </main>
     </div>
