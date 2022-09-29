49f8ea5b0c60b5c447e463145ffc59c4b2c5590d
qbentil <bentilshadrack72@gmail.com>
2022-09-29 18:08:57 +0000

    Update Categories: Implement Search

--- components/Searchbar/index.tsx
+++ components/Searchbar/index.tsx
@@ -2,7 +2,8 @@ import { FaSearch } from "react-icons/fa";
 import React from "react";
 
 interface Props{
-  className?: string
+  className?: string,
+  onSearch?: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 const SearchBar:React.FC<Props> = ({className}) => {
   return (
@@ -14,6 +15,7 @@ const SearchBar:React.FC<Props> = ({className}) => {
           id="search"
           className="border pr-1 pl-2  md:pl-5 border-gray-300 outline-none w-[80%] md:w-[90%] md:h-10 h-8 md:text-lg text-sm"
           placeholder="ENHANCED BY Google"
+          
         />
         <div className="h-8 md:h-10 w-[15%] md:w-[8%] items-center flex justify-start">
         <button className="bg-blue-600 p-2 text-white text-sm justify-center  items-center flex  h-[90%] w-full rounded-sm">
