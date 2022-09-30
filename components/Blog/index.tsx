86b12f0bca43e5a35262813971386f40d5862d7e
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-09-30 17:25:04 +0000

    feat: :sparkles: dynamic posts

--- components/Blog/index.tsx
+++ components/Blog/index.tsx
@@ -1,17 +1,36 @@
 import React from 'react'
 import SingleBlog from './item'
+import { useStateValue } from '../../context/StateProvider'
+import { Empty } from '../Promises'
 
-const blogs = [1, 5, 2, 4, 5, 6,]
 const Blog = () => {
-  return (
-        <div className='py-2 grid grid-flow-row grid-cols-1 md:grid-cols-2 gap-2'>
-        {            
-            blogs.map((b, index) => (
-                <SingleBlog key={index} position = {index} />
-            ))  
+  const [{ posts, categories }, dispatch] = useStateValue()
+  console.log(categories);
+  console.log(posts);
+  
+	return (
+		<div >
+      
+      {
+        posts.length > 0 ? (
+          <div className='py-2 grid grid-flow-row grid-cols-1 md:grid-cols-2 gap-4'>
+            {
+              posts && posts.slice(0, 8).map((post: any) => (
+                <SingleBlog key={post._id} data={post} />
+              ))
             }
-        </div>
-  )
+          </div>
+          
+        ) : (
+            <div className='mx-auto'>
+
+              <Empty text={'No posts yet. Watch out for more!'} />
+            </div>
+        )
+      }
+			
+		</div>
+	)
 }
 
 export default Blog
