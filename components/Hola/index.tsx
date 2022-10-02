027da6cc1e28423f47937fc6bf68c78cd6bb8ab3
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-10-02 13:39:22 +0000

    feat: :sparkles: minor changes

--- components/Hola/index.tsx
+++ components/Hola/index.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 interface Props{
     message: string
-    color?:string
+  color?: string
+  twColor?: string
 }
-const Hola:React.FC<Props> = ({message, color}) => {
+const Hola:React.FC<Props> = ({message, color, twColor}) => {
   return (
     <div className={`md:w-[80%] flex justify-start  items-center w-[90%] mt-6 -mb-10 md:my-2`}>
         <div className='md:w-[90%] lg:w-[80%] w-[100%] md:text-5xl text-xl  font-bold'>
-            <p className={`md:w-[70%] w-[80%]`}
+            <p className={`md:w-[70%] w-[80%] text-${twColor}`}
             style={{
               color: color || ""
             }}
