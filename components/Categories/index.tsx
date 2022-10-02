816ef1bea38229dc925e6970f305c5eb39e42444
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-10-02 11:14:41 +0000

    feat: :sparkles: fixed spelling error

--- components/Categories/index.tsx
+++ components/Categories/index.tsx
@@ -13,7 +13,7 @@ const Categories:React.FC<Props> = ({titlePosition}) => {
         <div className='w-full pb-20 bg-white grid grid-flow-row grid-cols-2 gap-1 md:grid-cols-5 lg:grid-cols-7 md:gap-2 col-start-auto p-2'>
         {
             categories.map(category => (
-                <Link  href={`cateory/${category.name}`} key={category._id}>
+                <Link  href={`category/${category.name}`} key={category._id}>
                     <p className='py-3 cursor-pointer border border-gray-200 text-blue-600 bg-gray-200 text-center hover:bg-gray-100'>{category.name}</p>
                 </Link>
             ))
