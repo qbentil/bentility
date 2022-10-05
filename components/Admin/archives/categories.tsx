2488af2afbc01fa82b7ce08925c005c382228da5
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-10-05 16:08:48 +0000

    feat: :sparkles: setup archive categories

---
+++ components/Admin/archives/categories.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ArchivedCategories = () => {
+  return (
+    <div>ArchivedCategories</div>
+  )
+}
+
+export default ArchivedCategories
---
+++ pages/admin/archives/categories.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Admin from "..";
+import ArchivedCategories from '../../../components/Admin/archives/categories';
+
+const AchivedCategories = () => {
+  return (
+    <Admin page={<ArchivedCategories />} />
+  )
+}
+
+export default AchivedCategories
