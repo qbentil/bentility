9ab17579483e0e3695f166335e6d5ed8c521c886
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-10-06 08:31:49 +0000

    feat: :sparkles: deleted redundant files

--- components/Admin/archives/categories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ArchivedCategories = () => {
-  return (
-    <div>ArchivedCategories</div>
-  )
-}
-
-export default ArchivedCategories
--- components/Admin/archives/posts.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ArchivedPosts = () => {
-  return (
-    <div>ArchivedPosts</div>
-  )
-}
-
-export default ArchivedPosts
