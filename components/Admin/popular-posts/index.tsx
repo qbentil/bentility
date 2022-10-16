36a4996ea6e9a831eb3d669465329086e63e4baf
qbentil <bentilshadrack72@gmail.com>
2022-10-16 01:15:13 +0000

    Filter drafts from popular posts and categories

--- components/Admin/popular-posts/index.tsx
+++ components/Admin/popular-posts/index.tsx
@@ -1,36 +1,36 @@
-import React from 'react'
-import { RiSoundModuleLine } from 'react-icons/ri'
-import Post from '../post'
-import { useStateValue } from '../../../context/StateProvider'
-import Link from 'next/link'
+import React from "react";
+import { RiSoundModuleLine } from "react-icons/ri";
+import Post from "../post";
+import { useStateValue } from "../../../context/StateProvider";
+import Link from "next/link";
 
 function PopularPosts() {
-	const [{ posts }, dispatch] = useStateValue()
-	return (
-		<div className='w-[60%] flex flex-col items-center px-5 my-4'>
-			{/* section head */}
-			<div className='bg-white w-full rounded-t-lg flex justify-between items-center py-2 px-4 border-b-2 border-gray-200'>
-        <p className=' text-active font-sans'>Popular Posts</p>
-        <Link href='/admin/posts'>
-        <div className='flex items-center justify-center gap-3 border hover:bg-active-bg ease-in-out duration-300 transition-colors border-active-bg py-2 px-4 font-sans cursor-pointer'>
-					<RiSoundModuleLine className='text-primary' />
-					<p className=' text-active text-sm'>View All</p>
-				</div>
+  const [{ posts }, dispatch] = useStateValue();
+  return (
+    <div className="w-[60%] flex flex-col items-center px-5 my-4">
+      {/* section head */}
+      <div className="bg-white w-full rounded-t-lg flex justify-between items-center py-2 px-4 border-b-2 border-gray-200">
+        <p className=" text-active font-sans">Popular Posts</p>
+        <Link href="/admin/posts">
+          <div className="flex items-center justify-center gap-3 border hover:bg-active-bg ease-in-out duration-300 transition-colors border-active-bg py-2 px-4 font-sans cursor-pointer">
+            <RiSoundModuleLine className="text-primary" />
+            <p className=" text-active text-sm">View All</p>
+          </div>
         </Link>
-				
-			</div>
-			{/* posts table */}
-			<div className='w-full flex flex-col bg-white pt-2 gap-2 overflow-y-auto h-[59vh]'>
-				{
-					
-					posts &&
-						posts
-							.slice(0, 8)
-							.map((post : any) => <Post post={post} key={post._id} />)
-				}
-			</div>
-		</div>
-	)
+      </div>
+      {/* posts table */}
+      <div className="w-full flex flex-col bg-white pt-2 gap-2 overflow-y-auto h-[59vh]">
+        {posts &&
+          posts
+            .slice(0, 8)
+            .map((post: any) => {
+				// return if post status is draft
+				if (post?.status === "draft") return;											
+				return <Post post={post} key={post._id} />
+			})}
+      </div>
+    </div>
+  );
 }
 
-export default PopularPosts
+export default PopularPosts;
