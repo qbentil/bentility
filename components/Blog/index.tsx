a372d4c7d3b5bea85c2cc9f76d69bed7d04f8c81
Bentil Shadrack <bentilshadrack72@gmail.com>
2022-10-19 16:59:27 +0000

    Filter post

--- components/Blog/index.tsx
+++ components/Blog/index.tsx
@@ -1,19 +1,20 @@
+import { Empty } from "../Promises";
 import React from "react";
 import SingleBlog from "./item";
 import { useStateValue } from "../../context/StateProvider";
-import { Empty } from "../Promises";
 
 const Blog = () => {
-  const [{ posts, categories }, dispatch] = useStateValue();
+  const [{ posts }, dispatch] = useStateValue();
 
   return (
     <div>
       {posts.length > 0 ? (
         <div className="py-2 grid grid-flow-row grid-cols-1 md:grid-cols-2 md:gap-4 gap-0">
           {posts &&
-            posts
-              .slice(0, 8)
-              .map((post: any) => <SingleBlog key={post._id} data={post} />)}
+            posts.slice(0, 8).map((post: any) => {
+              if (post.status !== "published") return null;
+              <SingleBlog key={post._id} data={post} />;
+            })}
         </div>
       ) : (
         <div className="mx-auto">
