af8a0ec05e5b1b80712d9356a87bba211f0eeece
qbentil <bentilshadrack72@gmail.com>
2022-12-09 14:46:19 +0000

    Update Axios base URL

--- util/axios.tsx
+++ util/axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const Axios =  axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
     withCredentials: true,
 })
 
